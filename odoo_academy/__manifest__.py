da4673b732997e36d147f9b8139e99638906ae04
Ariadna-arco <arco@odoo.com>
2021-09-28 22:45:58 +0000

    Adds new basic academy module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'sumary': """Academy app for training""",
+    'description': """
+        Academy training:
+        -Courses
+        -sessions
+    """,
+    'author': 'Ariadna',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
