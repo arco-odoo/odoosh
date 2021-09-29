9af23631fd697d4a66159b6e14018b93b869bce1
Ariadna-arco <arco@odoo.com>
2021-09-29 17:04:27 +0000

    Adds space mission module

---
+++ odoo_space_mission/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Space Mission',
+    'sumary': """Exciting space mission""",
+    'description': """
+        Odoo Space Mission will include coffee, snacks and a space ship
+    """,
+    'author': 'Ariadna',
+    'website': 'https://www.odoo.com',
+    'category': 'Space and time',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
---
+++ odoo_space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+class SpaceShip(models.Model):
+    _name = 'space_mission.space_ship',
+    _description = 'Space ship details'
+    
+    name = fields.Char(title='Name')
+    height = fields.Integer(string='Height')
+    width = fields.Integer(string='Width')
+    
