df7ac0ee22414eb77570b1c6965c1a9c69f17330
Ariadna-arco <arco@odoo.com>
2021-09-29 17:14:05 +0000

    New lines added and comma corrected

--- odoo_space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
+++ odoo_space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -3,7 +3,8 @@
 from odoo import fields, models, api
 
 class SpaceShip(models.Model):
-    _name = 'space_mission.space_ship',
+    
+    _name = 'space_mission.space_ship'
     _description = 'Space ship details'
     
     name = fields.Char(title='Name')
