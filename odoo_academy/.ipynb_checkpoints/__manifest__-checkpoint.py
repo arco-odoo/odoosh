aa29618b9b9e63f31e3117bbfc0d12b85fec29c4
Ariadna-arco <arco@odoo.com>
2021-10-01 20:40:57 +0000

    Adds inheritance and views

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -11,7 +11,14 @@
     'website': 'https://www.odoo.com',
     'category': 'Training',
     'version': '0.1',
-    'depends': ['base'],
-    'data': [],
+    'depends': ['sale'],
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml'
+    ],
     'demo': [],
 }
