4ef64c0788bf43650595e0d02205b78ca3f52459
Ariadna-arco <arco@odoo.com>
2021-10-04 17:14:08 +0000

    Adds wizard for create sale order from session

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -24,6 +24,15 @@ class Session(models.Model):
     end_date = fields.Date(string='End date', compute='_compute_end_date',
                            inverse='_inverse_end_date', store=True)
     
+    state = fields.Selection(string='States',
+                             selection=[('draft', 'Draft'),
+                                        ('open', 'In progress'),
+                                        ('done', 'Done'),
+                                        ('canceled', 'Canceled')],
+                             default='draft',
+                             required=True)
+    total_price = fields.Float(string='Total price', related='course_id.total_price')
+    
     @api.depends('start_date', 'end_date')
     def _compute_end_date(self):
         for record in self:
